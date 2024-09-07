c922eb7d1450dde1f15a786c5fb361698ef4ad80
Sunanda <rcsunanda@gmail.com>
2024-09-07 14:43:16 -0400

    Handle potential multiple tool calls in one LLM call, stop after max no. of calls

--- calculator_agent.py
+++ calculator_agent.py
@@ -7,13 +7,13 @@ import json
 from calculator import calculate
 
 
-api_key = os.environ.get("OPEN_AI_TOKEN")
-
-
 class CalculatorAgent:
     def __init__(self):
+        api_key = os.environ.get("OPEN_AI_TOKEN_2")
+
         self.client = openai.OpenAI(api_key=api_key)
-        self.model = 'gpt-3.5-turbo'
+        # self.model = 'gpt-3.5-turbo'
+        self.model = 'gpt-4o'
 
         self.system_prompt = "You are a calculator agent. Given a string describing a mathematical expression, " \
                              "you can determine the next *single* calculation step to be performed in the form of a function call to a calculate function. " \
@@ -22,13 +22,10 @@ class CalculatorAgent:
                              "The answer to each step will be calculated using a calculate function and given back to you." \
                              "At each step, output the next *single* function call necessary to calculate the result."
 
-    def run(self, expression: str):
-        print(f"Input expression: {expression}")
+        self.max_calls = 3
+        self.return_tool_call_msgs = True
 
-        initial_prompt = f'This is the mathematical expression to be evaluated: {expression}. ' \
-                 f'Perform the calculation step by step, making tool calls to the provided function.'
-
-        tools = [
+        self.tools = [
             {
                 "type": "function",
                 "function": {
@@ -51,22 +48,30 @@ class CalculatorAgent:
             },
         ]
 
+    def run(self, expression: str):
+        print(f"Input expression: {expression}")
+
+        initial_prompt = f'This is the mathematical expression to be evaluated: {expression}. ' \
+                 f'Perform the calculation step by step, making tool calls to the provided function.'
+
         messages = [
             {"role": "system", "content": self.system_prompt},
             {"role": "user", "content": initial_prompt},
         ]
 
         steps = []
+        i = 0
         final_result = None
 
         while True:
-            print('\n----- messages -----\n')
-            print(messages)
+            i += 1
+            # print('\n----- messages -----\n')
+            # print(messages)
 
             completion = self.client.chat.completions.create(
                 model=self.model,
                 messages=messages,
-                tools=tools,
+                tools=self.tools,
                 tool_choice="required",
             )
 
@@ -76,49 +81,64 @@ class CalculatorAgent:
                 print("Error: Expected a function call but received none.")
                 break
 
-            # Keep only the first element in the list, as we are expecting only one function call at a time
-            response.tool_calls = response.tool_calls[:1]
+            function_call_result_message = []
 
-            function_call = response.tool_calls[0].function
-            func_args = json.loads(function_call.arguments)
+            is_final_step = False
+            result = None
+            call_steps = []
 
-            a = func_args['a']
-            b = func_args['b']
-            op = func_args['op']
-            is_final_step = func_args['is_final_step']
+            for tool_call in response.tool_calls:
+                function_call = tool_call.function
+                func_args = json.loads(function_call.arguments)
 
-            try:
-                result = calculate(a, b, op)
-            except Exception as e:
-                print(f"Error in calculation: {e}")
-                return str(e)
+                a = func_args['a']
+                b = func_args['b']
+                op = func_args['op']
+                is_final_step = func_args['is_final_step']
 
-            step = f"{a} {op} {b} = {result}"
-            steps.append(step)
-            print(f"Step: {step}")
+                try:
+                    result = calculate(a, b, op)
+                except Exception as e:
+                    print(f"Error in calculation: {e}")
+                    return str(e)
+
+                step = f"{a} {op} {b} = {result}"
+                call_steps.append(step)
+                steps.append(step)
+
+                function_call_result_message.append({
+                    "role": "tool",
+                    "content": json.dumps({"result": result}),
+                    "tool_call_id": tool_call.id
+                })
+
+            print(f"Call {i}: {'    ,   '.join(call_steps)}")
 
             if is_final_step:
-                final_result = result
+                final_result = result if result is not None else final_result
                 print(f"Final result: {final_result}")
                 break
 
-            function_call_result_message = {
-                "role": "tool",
-                "content": json.dumps({"result": result}),
-                "tool_call_id": response.tool_calls[0].id
-            }
+            # next_prompt = "Proceed with the next step of the calculation, in the correct order of operations."
 
-            next_prompt = "Proceed with the next step of the calculation, in the correct order of operations."
+            steps_so_far = '\n'.join(steps)
+            next_prompt = f"Proceed with the next step of the calculation. For reference, the original expression is: {expression}. " \
+                          f"And the steps calculated so far are: {steps_so_far}."
 
-            # steps_so_far = '\n'.join(steps)
-            # next_prompt = f"Proceed with the next step of the calculation. For reference, the original expression is: {expression}. " \
-            #               f"And the steps calculated so far are: {steps_so_far}."
+            # Keep only the first element in the list, as we expect & handle only one function call at a time
+            # response.tool_calls = response.tool_calls[:1]
 
-            messages.extend([
-                response,
-                function_call_result_message,
-                {"role": "user", "content": next_prompt},
-            ])
+            if self.return_tool_call_msgs:
+                messages.extend([
+                    response,
+                    *function_call_result_message,
+                ])
+
+            messages.append({"role": "user", "content": next_prompt})
+
+            if i >= self.max_calls:
+                print(f"Max calls reached: {self.max_calls}")
+                break
 
         # print("Calculation steps:")
         # for step in steps:
--- test_script.py
+++ test_script.py
@@ -5,8 +5,9 @@ agent = CalculatorAgent()
 
 # ans = agent.run('10 + 5 * 3')
 
-# ans = agent.run('10 + 5 * 3 - 8 / 2 * 3 + 1')
+# ans = agent.run('10 + 5 * 3 - 8 / 2')
 
-ans = agent.run('10 + 5 * 3 - 8 / 2')
+ans = agent.run('10 + 5 * 3 - 8 / 2 * 3 + 1')
+
 
 print(ans)
